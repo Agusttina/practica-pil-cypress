253c1121fdd80a3bb14b42ebc3dba724ac354907
Agusttina <agus.gramajo@outlook.es>
2023-08-17 12:59:20 -0300

    agrego tests de clase

---
+++ cypress/e2e/EDEN/eden2.cy.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+const edenHome2 = require("../../Page/edenHome2");
+const edenHeader2 = require("../../Page/edenHeader2");
+
+describe("Test sobre la página de EDEN ENTRADAS", () => {
+  beforeEach(() => {
+    cy.openWeb();
+  });
+  it("Verificar subtitulos", () => {
+    edenHome2.getSubTitles().first().should("contain.text", "BUSCAR EVENTO");
+    edenHome2
+      .getSubTitles()
+      .last()
+      .should("contain.text", "CALENDARIO DE EVENTOS");
+  });
+
+  it("Verificar Menu", () => {
+    const menuBtn = [
+      "HOME",
+      "TODOS",
+      "AGENDA DEL FINDE",
+      "RECITALES",
+      "TEATROS",
+      "CUARTETOS",
+      "FESTIVALES",
+      "SALAS",
+    ];
+
+    menuBtn.forEach((txtBtn, $index) => {
+      edenHeader2.getMenuButtons().eq($index).should("contain.text", txtBtn);
+    });
+  });
+
+  it("Verificar pagina de recitales", () => {
+    edenHeader2.getMenuButtons().eq(3).click();
+  });
+});
