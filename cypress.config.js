e62390aaa91fcb5ef12cdab65f752353ba8f2a68
Agusttina <agus.gramajo@outlook.es>
2023-10-05 19:35:54 -0300

    guardado de archivos

--- cypress.config.js
+++ cypress.config.js
@@ -1,32 +1,51 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 
-module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    charts: true,
-    reportPageTitle: "Reporte personalizable Agustina",
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
-  },
-  env: {
-    grepFilterSpecs: true,
-    grepOmitFiltered: true,
-    viewportmobile: {
-      device: "iphone-xr",
+  module.exports = defineConfig({
+    env: {
+      TAGS: "not @ignore",
+      //Configuración de las URLs
+      YVYTU_URL: "https://vientosdelaselva.com.ar/",
+      EDEN_URL: "https://www.edenentradas.com.ar/",
+      CLARO_URL: "https://tienda.claro.com.ar/",
+      DEXTER_URL: "https://www.dexter.com.ar/",
+      STORE_URL: "https://automationteststore.com/",
+      HIPERLIBERTAD_URL: "https://www.hiperlibertad.com.ar/",
+      //Configuración de los usuarios
+      users: [
+        {
+          user: "XXXXXXXXXXXXXXXX",
+          pass: "XXXXXXXXXXXXXXXX",
+        },
+        {
+          user: "YYYYYYYYYYYYYYYYY",
+          pass: "YYYYYYYYYYYYYYYY",
+        },
+      ],
     },
-    viewportdesktop: {
-      device: "macbook-16",
+    e2e: {
+      hideXHRInCommandLog: true,
+      specPattern: "cypress/e2e/features/**/*.feature",
+      async setupNodeEvents(on, config) {
+        const bundler = createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        });
+        on("file:preprocessor", bundler);
+        await addCucumberPreprocessorPlugin(on, config);
+        //require("cypress-image-diff-js/dist/plugin")(on, config);
+        return config;
+      },
     },
-  },
-  e2e: {
-    baseUrl: "https://www.edenentradas.com.ar/",
-    setupNodeEvents(on, config) {
-      require("@bahmutov/cy-grep/src/plugin")(config);
-      require('cypress-mochawesome-reporter/plugin')(on);
-      require("cypress-image-diff-js/dist/plugin")(on, config);
-      // IMPORTANT: return the config object
-      return config;
+    viewportWith: 1536,
+    viewportHeight: 960,
+    video: false,
+    retries: {
+      openMode: 0,
+      runMode: 0,
     },
-  },
-});
+    defaultCommandTimeout: 8000,
+  });
